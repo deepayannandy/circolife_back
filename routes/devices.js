e9fdd7e08ca09a80c495228fc2d07dffebf833e1
Deepayan Nandy <deepayan.622@gmail.com>
2023-11-25 16:01:29 +0530

    more on devices

--- routes/devices.js
+++ routes/devices.js
@@ -44,4 +44,48 @@ router.get('/:id',async (req,res)=>{
     }
 })
 
+router.patch('/:id', getDevice,async(req,res)=>{
+    if(req.body.deviceName!=null){
+        res.dev.deviceName=req.body.deviceName;
+    }
+    if(req.body.isadmin!=null){
+        res.dev.isadmin=req.body.isadmin;
+    }
+    try{
+        const newDev=await res.dev.save()
+        res.status(201).json({"_id":newDev.id})
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+router.delete("/:id",async (req,res)=>{
+    console.log("Deleting Device: "+req.params.id)
+    dev=await devices.findById(req.params.id)
+        if(dev==null){
+            return res.status(404).json({message:"Device unavailable!"})
+        }
+    try{
+        const reasult= await devices.deleteOne({_id: new mongodb.ObjectId(req.params.id)})
+        res.json(reasult)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//middleware
+async function getDevice(req,res,next){
+    let dev
+    try{
+        dev=await devices.findById(req.params.id)
+        if(dev==null){
+            return res.status(404).json({message:"User unavailable!"})
+        }
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+    res.dev=dev
+    next()
+}
+
 module.exports=router
